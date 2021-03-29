8999b9ecd101dd680adf5bcf827507c81eb7e9a7
Aya821 <a.w.a.w.8532@icloud.com>
2021-03-29 17:25:22 +0900

    Initial Commit

---
+++ Count1Shion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Count1Shion
+//
+//  Created by A.watanabe on 2021/03/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
